4658523268104daf01d58a78a02ffa0a871b2663
Lalon Sheikh <you@example.com>
2024-06-09 21:23:59 +0600

    env added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBnKKzAPBotREwmuXtTIZqqf0k-UdOmDD8",
-    authDomain: "b9a9-real-estate-assignment.firebaseapp.com",
-    projectId: "b9a9-real-estate-assignment",
-    storageBucket: "b9a9-real-estate-assignment.appspot.com",
-    messagingSenderId: "319731862974",
-    appId: "1:319731862974:web:f5328d15a3c404dafebdef"
+    apiKey:import.meta.env.VITE_APIKEY,
+    authDomain:import.meta.env.VITE_AUTHDOMAIN,
+    projectId:import.meta.env.VITE_PROJECTID,
+    storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+    appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
